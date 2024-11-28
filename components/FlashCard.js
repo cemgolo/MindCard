201581755a27daa4ece446e5c1133cd16aedf785
ikram uygur <123154240+marky731@users.noreply.github.com>
2024-11-28 11:45:19 +0100

    Seperated rating buttons from flash card

--- components/FlashCard.js
+++ components/FlashCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import RatingButtons from './RatingButtons';
 
 const FlashCard = ({ front, back, flipped, onFlip, onRate }) => {
   return (
@@ -12,23 +13,7 @@ const FlashCard = ({ front, back, flipped, onFlip, onRate }) => {
           <Text style={styles.flipButtonText}>Flip card</Text>
         </TouchableOpacity>
       ) : (
-        <View style={styles.ratingContainer}>
-          <Text style={styles.ratingTitle}>Rate your recall performance</Text>
-          <View style={styles.ratingButtons}>
-            <TouchableOpacity style={styles.failButton} onPress={onRate}>
-              <Text style={styles.buttonText}>Fail</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.slowButton} onPress={onRate}>
-              <Text style={styles.buttonText}>Slow</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.averageButton} onPress={onRate}>
-              <Text style={styles.buttonText}>Average</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.fastButton} onPress={onRate}>
-              <Text style={styles.buttonText}>Fast</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <RatingButtons onRate={onRate} />
       )}
     </View>
   );
@@ -54,10 +39,10 @@ const styles = StyleSheet.create({
     width: '80%',
     height: '50%',
     justifyContent: 'center', 
-    alignItems: 'center', // Centers text horizontally
+    alignItems: 'center', 
   },
   cardText: {
-    fontSize: 32, // Set a larger default size
+    fontSize: 32, 
     fontWeight: 'bold',
     textAlign: 'center',
     flexShrink: 1, 
@@ -73,58 +58,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'center',
-    width: '100%',            // Ensures the text takes full width of the container
-    textAlignVertical: 'center',
-  },
-  ratingContainer: {
-    marginTop: 20,
-    alignItems: 'center',
-  },
-  ratingTitle: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  ratingButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '80%',
-  },
-  failButton: {
-    width: 70, 
-    height: 40, 
-    padding: 10,
-    backgroundColor: '#ff6666',
-    borderRadius: 5,
-    marginHorizontal: 5,  
-  },
-  slowButton: {
-    width: 70, 
-    height: 40, 
-    padding: 10,
-    backgroundColor: '#ffd966',
-    borderRadius: 5,
-    marginHorizontal: 5,  
-  },
-  averageButton: {
-    width: 70, 
-    height: 40, 
-    padding: 10,
-    backgroundColor: '#00bbff',
-    borderRadius: 5,
-    marginHorizontal: 5,  
-  },
-  fastButton: {
-    width: 70, 
-    height: 40, 
-    padding: 10,
-    backgroundColor: '#66cc66',
-    borderRadius: 5,
-    marginHorizontal: 5,  
-  },
-  buttonText: {
-    color: '#000',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    width: '100%',
     textAlignVertical: 'center',
   },
 });
---
+++ components/RatingButtons.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const RatingButtons = ({ onRate }) => {
+    return (
+        <View style={styles.ratingContainer}>
+          <Text style={styles.ratingTitle}>Rate your recall performance</Text>
+          <View style={styles.ratingButtons}>
+            <TouchableOpacity style={styles.failButton} onPress={onRate}>
+              <Text style={styles.buttonText}>Fail</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.slowButton} onPress={onRate}>
+              <Text style={styles.buttonText}>Slow</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.averageButton} onPress={onRate}>
+              <Text style={styles.buttonText}>Average</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.fastButton} onPress={onRate}>
+              <Text style={styles.buttonText}>Fast</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+ratingContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  ratingTitle: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  ratingButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '80%',
+  },
+  failButton: {
+    width: 70, 
+    height: 40, 
+    padding: 10,
+    backgroundColor: '#ff6666',
+    borderRadius: 5,
+    marginHorizontal: 5,  
+  },
+  slowButton: {
+    width: 70, 
+    height: 40, 
+    padding: 10,
+    backgroundColor: '#ffd966',
+    borderRadius: 5,
+    marginHorizontal: 5,  
+  },
+  averageButton: {
+    width: 70, 
+    height: 40, 
+    padding: 10,
+    backgroundColor: '#00bbff',
+    borderRadius: 5,
+    marginHorizontal: 5,  
+  },
+  fastButton: {
+    width: 70, 
+    height: 40, 
+    padding: 10,
+    backgroundColor: '#66cc66',
+    borderRadius: 5,
+    marginHorizontal: 5,  
+  },
+  buttonText: {
+    color: '#000',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+});
+
+export default RatingButtons;
