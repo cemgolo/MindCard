989c4619e9dc99ba712dddceac41503973c6f89a
Thiibo <40576021+Thiibo@users.noreply.github.com>
2024-12-08 18:14:15 +0100

    Create `ADD_EMPTY_DECK` action

--- storage/helper.ts
+++ storage/helper.ts
@@ -1,5 +1,5 @@
 import { createEmptyCard, State } from "ts-fsrs";
-import { FlashCard, FlashCardSide } from "./types";
+import { FlashCard, FlashCardDeck, FlashCardSide } from "./types";
 
 function createCard(
     front: FlashCardSide | string,
@@ -14,9 +14,22 @@ function createCard(
     }
 }
 
-function isDue(card: FlashCard) {
+function createDeck(name: string, cards: FlashCard[] = []): FlashCardDeck {
+    return {
+        name: name,
+        maxCardsEverySession: {
+            [State.New]:        5,
+            [State.Learning]:   Infinity,
+            [State.Review]:     20,
+            [State.Relearning]: Infinity
+        },
+        cards: cards
+    };
+}
+
+function isDue(card: FlashCard): boolean {
     const dueDate = typeof card.due === "string" ? Date.parse(card.due) : card.due;
     return dueDate <= new Date();
 }
 
-export { createCard, isDue }
+export { createCard, createDeck, isDue }
--- storage/reducer.js
+++ storage/reducer.js
@@ -1,36 +1,17 @@
-import { State } from "ts-fsrs";
-import { ADD_DECK } from "./actions";
-import { createCard } from "./helper";
-
-/** @type import("./types").FlashCardDeck **/
-const defaultDeckOptions = {
-    maxCardsEverySession: {
-        [State.New]:        5,
-        [State.Learning]:   Infinity,
-        [State.Review]:     20,
-        [State.Relearning]: Infinity
-    }
-};
+import { ADD_DECK, ADD_EMPTY_DECK, EDIT_DECK } from "./actions";
+import { createCard, createDeck } from "./helper";
 
 /** @type import("./types").ReduxDeckState **/
 const initialState = {
     decks: [
-        {
-            ...defaultDeckOptions,
-            name: 'Fruit vocab',
-            cards: [
-                createCard("Apple", "A red fruit."),
-                createCard("Banana", "A yellow fruit."),
-            ]
-        },
-        {
-            ...defaultDeckOptions,
-            name: 'Rocket science',
-            cards: [
-                createCard("Vectors", "starts with V"),
-                createCard("Rocket", "starts with S"),
-            ]
-        }
+        createDeck("Fruit vocab", [
+            createCard("Apple", "A red fruit."),
+            createCard("Banana", "A yellow fruit.")
+        ]),
+        createDeck("Rocket science", [
+            createCard("Vectors", "starts with V"),
+            createCard("Rocket", "starts with S")
+        ]),
     ]
 };
 
@@ -42,6 +23,14 @@ const deckReducer = (state = initialState, action) => {
                 decks: [ ...state.decks, action.payload ]
             };
         }
+
+        case ADD_EMPTY_DECK: {
+            return {
+                ...state,
+                decks: [ ...state.decks, createDeck(action.payload) ]
+            }
+        }
+
         default:
             return state;
     }
