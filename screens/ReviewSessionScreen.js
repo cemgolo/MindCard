bf743988b69a6fa8a92f9d330fce4262b038cfa0
Thiibo <40576021+Thiibo@users.noreply.github.com>
2024-12-10 16:24:00 +0100

    Fix some issues in `ReviewSessionScreen`

--- screens/ReviewSessionScreen.js
+++ screens/ReviewSessionScreen.js
@@ -3,52 +3,60 @@ import { View, Text, StyleSheet } from 'react-native';
 import FlashCard from '../components/FlashCard';
 import { useDispatch, useSelector } from 'react-redux';
 import { FSRS } from 'ts-fsrs';
-import { generateSessionCards, isDue } from '../storage/helper';
+import { cardReviewRatings, generateSessionCards, isDue } from '../storage/helper';
 import { updateCard } from '../storage/actions';
 import { randomObjectValue } from '../scripts/arrays';
+
+function pickRandomCard(cards) {
+  const cardsOfRandomState = randomObjectValue(cards);
+  return cardsOfRandomState[Math.floor(Math.random() * cardsOfRandomState.length)];
+}
+
+function removeCardFromSession(sessionCards, card) {
+  const {[card.state]: _, ...otherStateCards} = sessionCards;
+  const newStateCards = sessionCards[card.state].filter(item => item.uuid !== card.uuid);
+  if (newStateCards.length <= 0) {
+    return otherStateCards;
+  } else {
+    return { ...otherStateCards, [card.state]: newStateCards }
+  }
+}
+
+function addCardToSession(sessionCards, card) {
+  if (!(card.state in sessionCards)) sessionCards[card.state] = [];
+  sessionCards[card.state].push(card)
+  return sessionCards;
+}
 
 const ReviewSessionScreen = ({ route, navigation }) => {
   const { deckName } = route.params;
   const deck = useSelector(state => state.decks.find(deck => deck.name === deckName));
   const [sessionCards, setSessionCards] = useState(generateSessionCards(deck));
+  const [userRatings, setUserRatings] = useState(cardReviewRatings.reduce((acc, rating) => ({...acc, [rating.label]: 0}), {}));
 
   const fsrs = new FSRS();
   const dispatch = useDispatch();
-  
-  const pickRandomCard = () => {
-    const cardsOfRandomState = randomObjectValue(sessionCards);
-    return cardsOfRandomState[Math.floor(Math.random() * cardsOfRandomState.length)];
-  }
-  const removeCardFromSession = (card) => {
-    const {[card.state]: _, otherStateCards} = sessionCards;
-    const newStateCards = sessionCards[card.state].filter(item => item.uuid !== card.uuid);
-    
-    if (newStateCards.length <= 0) {
-      setSessionCards(otherStateCards);
-    } else {
-      setSessionCards({ [card.state]: newStateCards, ...otherStateCards })
-    }
-  }
-  const addCardToSession = (card) => {
-    if (!(card.state in sessionCards)) sessionCards[card.state] = [];
-    sessionCards[card.state].push(card)
-    setSessionCards(sessionCards);
-  }
 
-  const [currentCard, setCurrentCard] = useState(pickRandomCard());
+  const [currentCard, setCurrentCard] = useState(pickRandomCard(sessionCards));
   const [flipped, setFlipped] = useState(false); // Manage flipped state here
 
   const handleRating = (rating) => {
-    removeCardFromSession(currentCard);
+    const ratingLabel = cardReviewRatings.find(item => item.action === rating).label;
+    const newRatings = { ...userRatings, [ratingLabel]: userRatings[ratingLabel] + 1 };
+    setUserRatings(newRatings);
+
+    let newSessionCards = removeCardFromSession(sessionCards, currentCard);
+
     const newCard = fsrs.repeat(currentCard, new Date())[rating].card;
     dispatch(updateCard(newCard));
-    if (isDue(newCard)) addCardToSession(newCard);
+    if (isDue(newCard)) newSessionCards = addCardToSession(newSessionCards, newCard);
 
-    if (Object.keys(sessionCards).length > 0) {
-      setCurrentCard(pickRandomCard());
+    if (Object.keys(newSessionCards).length == 0) {
+      setSessionCards(newSessionCards);
+      setCurrentCard(pickRandomCard(newSessionCards));
       setFlipped(false); // Reset flipped state when moving to the next card
     } else {
-      navigation.navigate('ReviewSessionEndScreen', { deckName });
+      navigation.navigate('ReviewSessionEndScreen', { deckName, userRatings: newRatings });
     }
   };
 
